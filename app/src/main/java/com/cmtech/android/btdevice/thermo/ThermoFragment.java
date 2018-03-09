3ade004a9ff02a1315e012ebc3fc9e75a66e6bc8
chenmtech <chenm91@163.com>
2018-03-10 07:46:26 +0800

    添加了ByteUtil
    负责实现byte[]与基本类型之间的转换；

--- app/src/main/java/com/cmtech/android/btdevice/thermo/ThermoFragment.java
+++ app/src/main/java/com/cmtech/android/btdevice/thermo/ThermoFragment.java
@@ -20,6 +20,7 @@ import com.cmtech.android.btdeviceapp.R;
 import com.cmtech.android.btdeviceapp.fragment.DeviceFragment;
 import com.cmtech.android.btdeviceapp.activity.MainActivity;
 import com.cmtech.android.btdeviceapp.model.ConfiguredDevice;
+import com.cmtech.android.btdeviceapp.util.ByteUtil;
 
 import static com.cmtech.android.btdevice.thermo.ThermoGattSerialExecutor.*;
 
@@ -40,7 +41,7 @@ public class ThermoFragment extends DeviceFragment {
             if (msg.what == MSG_THERMODATA) {
                 if (msg.obj != null) {
                     byte[] data = (byte[]) msg.obj;
-                    double temp = bytes2int(new byte[]{0x00, 0x00, data[1], data[0]})/100.0;
+                    double temp = ByteUtil.getInt(new byte[]{data[0], data[1], 0x00, 0x00})/100.0;
                     String str = String.format("%.2f", temp);
                     tvThermoData.setText(str);
                 }
@@ -188,26 +189,4 @@ public class ThermoFragment extends DeviceFragment {
         serialExecutor.stopExecuteCommand();
     }
 
-    //高位在前，低位在后
-    public static byte[] int2bytes(int num){
-        byte[] result = new byte[4];
-        result[0] = (byte)((num >>> 24) & 0xff);//说明一
-        result[1] = (byte)((num >>> 16)& 0xff );
-        result[2] = (byte)((num >>> 8) & 0xff );
-        result[3] = (byte)((num >>> 0) & 0xff );
-        return result;
-    }
-
-    //高位在前，低位在后
-    public static int bytes2int(byte[] bytes){
-        int result = 0;
-        if(bytes.length == 4){
-            int a = (bytes[0] & 0xff) << 24;//说明二
-            int b = (bytes[1] & 0xff) << 16;
-            int c = (bytes[2] & 0xff) << 8;
-            int d = (bytes[3] & 0xff);
-            result = a | b | c | d;
-        }
-        return result;
-    }
 }
