13a96e0268cc4e0aa3174cedd98b8fd938156edc
chenmtech <1276094137@qq.com>
2018-10-23 17:41:28 +0800

    实现心电信号回放功能；

--- app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/EcgReplayActivity.java
+++ app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/EcgReplayActivity.java
@@ -1,6 +1,9 @@
 package com.cmtech.android.bledevice.ecgmonitor;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -25,9 +28,13 @@ import java.util.List;
 public class EcgReplayActivity extends AppCompatActivity {
     private static final String TAG = "EcgReplayActivity";
 
+    private static final int MSG_START_REPLAY = 0;
+    private static final int MSG_SHOW_DATA = 1;
+
     private List<File> fileList = new ArrayList<>();
     private EcgFileAdapter fileAdapter;
     private RecyclerView rvFileList;
+    private BmeFile selectedFile;
 
     private WaveView ecgView;
 
@@ -37,33 +44,27 @@ public class EcgReplayActivity extends AppCompatActivity {
     private float viewXGridTime = 0.04f;          // 设置ECG View中的横向每小格代表0.04秒，即25格/s，这是标准的ECG走纸速度
     private float viewYGridmV = 0.1f;             // 设置ECG View中的纵向每小格代表0.1mV
 
-    private class ReplayThread extends Thread {
-        private int interval;
-        private BmeFile file;
+    private int interval = 10;
 
-        public ReplayThread(BmeFile file) {
-            this.file = file;
-            interval = 1000/file.getFs();
-        }
-
+    private Thread showThread = new Thread(new Runnable() {
         @Override
         public void run() {
-            while(true) {
+            while (true) {
                 try {
-                    ecgView.addData(file.readData());
-                    try {
-                        sleep(interval);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                    synchronized (EcgReplayActivity.this) {
+                        if (selectedFile != null) {
+                            ecgView.addData(selectedFile.readData());
+                        }
                     }
+                    Thread.sleep(interval);
                 } catch (FileException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
-    }
-
-    private ReplayThread workThread;
+    });
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -90,36 +91,52 @@ public class EcgReplayActivity extends AppCompatActivity {
 
         ecgView = findViewById(R.id.ecg_view);
 
+        showThread.start();
     }
 
-    public void replayEcgFile(File file) {
-        if(workThread != null && workThread.isAlive()) {
-            workThread.interrupt();
-
-        }
-
+    public synchronized void replayEcgFile(File file) {
         try {
-            BmeFile ecgFile = BmeFile.openBmeFile(file.getCanonicalPath());
-
-            //ViseLog.e(ecgFile.getBmeFileHead().toString());
-
-            int sampleRate = ecgFile.getFs();
-            int value1mV = ((BmeFileHead30)ecgFile.getBmeFileHead()).getValue1mV();
-            int xRes = Math.round(viewGridWidth / (viewXGridTime * sampleRate));   // 计算横向分辨率
-            float yRes = value1mV * viewYGridmV / viewGridWidth;                     // 计算纵向分辨率
-            ecgView.setRes(xRes, yRes);
-            ecgView.setGridWidth(viewGridWidth);
-            ecgView.setZeroLocation(0.5);
-            ecgView.clearView();
-            ecgView.startShow();
-
-            workThread = new ReplayThread(ecgFile);
-            workThread.start();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+            if (selectedFile != null) {
+                selectedFile.close();
+            }
+            selectedFile = BmeFile.openBmeFile(file.getCanonicalPath());
+            interval = 1000/selectedFile.getFs();
+            initialEcgView();
         } catch (FileException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initialEcgView() {
+        if(selectedFile == null) return;
+        int sampleRate = selectedFile.getFs();
+        int value1mV = ((BmeFileHead30)selectedFile.getBmeFileHead()).getValue1mV();
+        int xRes = Math.round(viewGridWidth / (viewXGridTime * sampleRate));   // 计算横向分辨率
+        float yRes = value1mV * viewYGridmV / viewGridWidth;                     // 计算纵向分辨率
+        ecgView.setRes(xRes, yRes);
+        ecgView.setGridWidth(viewGridWidth);
+        ecgView.setZeroLocation(0.5);
+        ecgView.clearView();
+        ecgView.startShow();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(selectedFile != null) {
+            try {
+                selectedFile.close();
+            } catch (FileException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(showThread != null && showThread.isAlive()) {
+            showThread.interrupt();
         }
     }
 }
