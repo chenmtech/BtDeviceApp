858aa183bd77b86aded1cdb16fc5145beb4ff16a
chenmtech <1276094137@qq.com>
2018-11-26 09:27:35 +0800

    Revert "Revert "MainActivity中对Dev-Frag的管理彻底交给了FragAndTabManager;""
    This reverts commit edde62b03fbe0592cf8beb7b6d95124ac268852f.

--- app/src/main/java/com/cmtech/android/bledevicecore/model/IBleDeviceActivity.java
+++ app/src/main/java/com/cmtech/android/bledevicecore/model/IBleDeviceActivity.java
@@ -1,8 +1,6 @@
 package com.cmtech.android.bledevicecore.model;
 
 public interface IBleDeviceActivity {
-    // 由Fragment获取相应的控制器
-    DeviceFragmentPair getDevFragPair(BleDeviceFragment fragment);
     // 关闭Fragment对应的设备，并销毁Fragment
     void closeDevice(BleDeviceFragment fragment);
 }
