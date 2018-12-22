5f244ee43da2659429abb34052e14ef0b442f02d
chenmtech <chenm91@163.com>
2018-12-23 06:25:08 +0800

    创建EcgSignalProcessor，把所有心电信号处理功能都放进来；

---
+++ app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/model/ecgProcess/EcgSignalProcessor.java
@@ -0,0 +1,32 @@
+package com.cmtech.android.bledevice.ecgmonitor.model.ecgProcess;
+
+import com.cmtech.android.bledevice.ecgmonitor.model.ecgProcess.ecgcalibrator.EcgCalibrator65536;
+import com.cmtech.android.bledevice.ecgmonitor.model.ecgProcess.ecgcalibrator.IEcgCalibrator;
+import com.cmtech.android.bledevice.ecgmonitor.model.ecgProcess.ecgfilter.IEcgFilter;
+import com.cmtech.android.bledevice.ecgmonitor.model.ecgProcess.ecghrprocess.IEcgHrProcessor;
+import com.cmtech.msp.qrsdetbyhamilton.QrsDetector;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * EcgSignalProcessor: 心电信号处理器，包含需要对心电信号做的所有处理
+ * Created by Chenm, 2018-12-23
+ */
+
+public class EcgSignalProcessor {
+    private IEcgCalibrator ecgCalibrator;
+    private IEcgFilter ecgFilter;
+    private QrsDetector qrsDetector;
+    private List<IEcgHrProcessor> hrProcessors = new ArrayList<>();
+
+    private EcgSignalProcessor() {
+        
+    }
+
+    // 构建者
+    public static class Builder {
+
+    }
+
+}
