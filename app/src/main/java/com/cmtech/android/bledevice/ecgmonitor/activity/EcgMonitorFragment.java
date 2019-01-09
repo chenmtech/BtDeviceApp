c3debb957518a9cc1b2cbe09b1bdc2c5cb8aa318
chenmtech <chenm91@163.com>
2019-01-09 10:48:03 +0800

    把EcgMonitorFragment中的心率直方图Chart派生为一个类；

--- app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/activity/EcgMonitorFragment.java
+++ app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/activity/EcgMonitorFragment.java
@@ -19,6 +19,7 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.cmtech.android.bledevice.ecgmonitor.model.EcgHrHistogram;
 import com.cmtech.android.bledevice.ecgmonitor.model.EcgMonitorDevice;
 import com.cmtech.android.bledevice.ecgmonitor.model.EcgMonitorDeviceConfig;
 import com.cmtech.android.bledevice.ecgmonitor.model.EcgMonitorState;
@@ -32,20 +33,6 @@ import com.cmtech.android.bledeviceapp.util.DateTimeUtil;
 import com.cmtech.android.bledevice.core.BleDeviceFragment;
 import com.cmtech.dsp.seq.RealSeq;
 import com.cmtech.dsp.util.SeqUtil;
-import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IValueFormatter;
-import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.vise.log.ViseLog;
 
 import java.util.ArrayList;
@@ -63,34 +50,25 @@ import static android.app.Activity.RESULT_OK;
 public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitorObserver {
     private static final String TAG = "EcgMonitorFragment";
 
-    private TextView tvEcgSampleRate;
-    private TextView tvEcgLeadType;
-    private TextView tvEcg1mV;
-    private TextView tvEcgHr;
-    private TextView tvEcgRecordTime;
-    private ScanWaveView ecgView;
-    private ImageButton ibSwitchSampleEcg;
-    private ImageButton ibRecord;
-    private CheckBox cbEcgFilter;
-    private CheckBox cbIsRest;
+    private TextView tvSampleRate; // 采样率
+    private TextView tvLeadType; // 导联类型
+    private TextView tvValue1mV; // 1mV定标值
+    private TextView tvHeartRate; // 心率值
+    private TextView tvRecordTime; // 记录时间
+    private ImageButton ibSwitchSampleStatus; // 切换采样状态
+    private ImageButton ibRecord; // 切换记录状态
+    private CheckBox cbIsFilter; // 是否滤波
+    private CheckBox cbIsRest; // 是否处于安静状态
+    private TextView tvTotalBeatTimes; // 总心跳次数
+    private ImageButton ibResetHistogram; // 重置心率直方图
+    private ScanWaveView ecgView; // 心电波形View
     private FrameLayout flEcgView;
     private RelativeLayout rlHrStatistics;
-    private BarChart hrBarHistogram;
-    private TextView tvHrTotal;
-    private ImageButton ibResetHistogram;
-
-
-    private BarDataSet hrBarDateSet;
-    private List<BarEntry> hrBarEntries = new ArrayList<>();
-    private List<String> hrBarXStrings = new ArrayList<>();
-
-    // 标记异常留言的Button
-    private List<Button> commentBtnList = new ArrayList<>();
+    private List<Button> commentBtnList = new ArrayList<>(); // 标记留言的Button
     int[] commentBtnId = new int[]{R.id.btn_ecgfeel_0, R.id.btn_ecgfeel_1, R.id.btn_ecgfeel_2};
-
-    private EcgMonitorDevice device;                // 保存设备模型
-
     private AudioTrack audioTrack;
+    private EcgMonitorDevice device; // 设备
+    private EcgHrHistogram hrHistogram; // 心率直方图
 
     public EcgMonitorFragment() {
 
@@ -106,7 +84,6 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
         super.onCreateView(inflater, container, savedInstanceState);
 
         device = (EcgMonitorDevice) getDevice();
-        //device.registerEcgMonitorObserver(EcgMonitorFragment.this);
 
         return inflater.inflate(R.layout.fragment_ecgmonitor, container, false);
     }
@@ -115,33 +92,33 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tvEcgSampleRate = view.findViewById(R.id.tv_ecg_samplerate);
-        tvEcgSampleRate.setText(String.valueOf(device.getSampleRate()));
+        tvSampleRate = view.findViewById(R.id.tv_ecg_samplerate);
+        tvSampleRate.setText(String.valueOf(device.getSampleRate()));
 
-        tvEcgLeadType = view.findViewById(R.id.tv_ecg_leadtype);
-        tvEcgLeadType.setText(String.format("L%s", device.getLeadType().getDescription()));
+        tvLeadType = view.findViewById(R.id.tv_ecg_leadtype);
+        tvLeadType.setText(String.format("L%s", device.getLeadType().getDescription()));
 
-        tvEcg1mV = view.findViewById(R.id.tv_ecg_1mv);
-        tvEcg1mV.setText(String.format(Locale.getDefault(), "%d/%d", device.getValue1mVBeforeCalibrate(), device.getValue1mVAfterCalibrate()));
+        tvValue1mV = view.findViewById(R.id.tv_ecg_1mv);
+        tvValue1mV.setText(String.format(Locale.getDefault(), "%d/%d", device.getValue1mVBeforeCalibrate(), device.getValue1mVAfterCalibrate()));
 
-        tvEcgHr = view.findViewById(R.id.tv_ecg_hr);
-        tvEcgHr.setText(String.valueOf(0));
-        tvEcgHr.setOnClickListener(new View.OnClickListener() {
+        tvHeartRate = view.findViewById(R.id.tv_ecg_hr);
+        tvHeartRate.setText(String.valueOf(0));
+        tvHeartRate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hrStatistics();
+                analysisHrStatistics();
             }
         });
 
         ecgView = view.findViewById(R.id.rwv_ecgview);
         updateEcgView(device.getxPixelPerData(), device.getyValuePerPixel(), device.getPixelPerGrid());
 
-        tvEcgRecordTime = view.findViewById(R.id.tv_ecg_recordtime);
-        tvEcgRecordTime.setText(DateTimeUtil.secToTime(device.getRecordSecond()));
+        tvRecordTime = view.findViewById(R.id.tv_ecg_recordtime);
+        tvRecordTime.setText(DateTimeUtil.secToTime(device.getRecordSecond()));
 
-        ibSwitchSampleEcg = view.findViewById(R.id.ib_ecgreplay_startandstop);
+        ibSwitchSampleStatus = view.findViewById(R.id.ib_ecgreplay_startandstop);
         updateState(device.getState());
-        ibSwitchSampleEcg.setOnClickListener(new View.OnClickListener() {
+        ibSwitchSampleStatus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 device.switchSampleState();
@@ -176,9 +153,9 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
             }
         });
 
-        cbEcgFilter = view.findViewById(R.id.cb_ecg_filter);
-        cbEcgFilter.setChecked(device.isFilter());
-        cbEcgFilter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        cbIsFilter = view.findViewById(R.id.cb_ecg_filter);
+        cbIsFilter.setChecked(device.isFilter());
+        cbIsFilter.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 device.setFilter(b);
@@ -199,20 +176,16 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
         });
 
         flEcgView = view.findViewById(R.id.fl_ecgview);
-        rlHrStatistics = view.findViewById(R.id.rl_hrstatistics);
-        hrBarHistogram = view.findViewById(R.id.bc_hr_histogram);
-        initBarChart(hrBarHistogram);
-        updateHrBarData(device.getHrStatistics());
-        hrBarDateSet = initBarDataSet("心率值统计", Color.BLUE, Color.BLACK);
-        showBarChart();
-        tvHrTotal = view.findViewById(R.id.tv_hr_totaltimes);
+        rlHrStatistics = view.findViewById(R.id.rl_hr_statistics);
+        hrHistogram = view.findViewById(R.id.bc_hr_histogram);
+        tvTotalBeatTimes = view.findViewById(R.id.tv_hr_totaltimes);
 
         ibResetHistogram = view.findViewById(R.id.ib_reset_histogram);
         ibResetHistogram.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 device.resetHrStatistics();
-                updateBarChart(null);
+                updateHrHistogram(null);
             }
         });
 
@@ -252,29 +225,29 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
     @Override
     public void updateState(final EcgMonitorState state) {
         if(state.canStart()) {
-            ibSwitchSampleEcg.setImageDrawable(ContextCompat.getDrawable(MyApplication.getContext(), R.mipmap.ic_ecg_play_48px));
-            ibSwitchSampleEcg.setClickable(true);
+            ibSwitchSampleStatus.setImageDrawable(ContextCompat.getDrawable(MyApplication.getContext(), R.mipmap.ic_ecg_play_48px));
+            ibSwitchSampleStatus.setClickable(true);
         } else if(state.canStop()) {
-            ibSwitchSampleEcg.setImageDrawable(ContextCompat.getDrawable(MyApplication.getContext(), R.mipmap.ic_ecg_pause_48px));
-            ibSwitchSampleEcg.setClickable(true);
+            ibSwitchSampleStatus.setImageDrawable(ContextCompat.getDrawable(MyApplication.getContext(), R.mipmap.ic_ecg_pause_48px));
+            ibSwitchSampleStatus.setClickable(true);
         } else {
-            ibSwitchSampleEcg.setClickable(false);
+            ibSwitchSampleStatus.setClickable(false);
         }
     }
 
     @Override
     public void updateSampleRate(final int sampleRate) {
-        tvEcgSampleRate.setText(String.valueOf(sampleRate));
+        tvSampleRate.setText(String.valueOf(sampleRate));
     }
 
     @Override
     public void updateLeadType(final EcgLeadType leadType) {
-        tvEcgLeadType.setText(String.format("L%s", leadType.getDescription()));
+        tvLeadType.setText(String.format("L%s", leadType.getDescription()));
     }
 
     @Override
     public void updateCalibrationValue(final int calibrationValueBefore, final int calibrationValueAfter) {
-        tvEcg1mV.setText(String.format(Locale.getDefault(), "%d/%d", calibrationValueBefore, calibrationValueAfter));
+        tvValue1mV.setText(String.format(Locale.getDefault(), "%d/%d", calibrationValueBefore, calibrationValueAfter));
     }
 
     @Override
@@ -305,12 +278,12 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
 
     @Override
     public void updateRecordSecond(final int second) {
-        tvEcgRecordTime.setText(DateTimeUtil.secToTime(second));
+        tvRecordTime.setText(DateTimeUtil.secToTime(second));
     }
 
     @Override
     public void updateEcgHr(final int hr) {
-        tvEcgHr.setText(String.valueOf(hr));
+        tvHeartRate.setText(String.valueOf(hr));
     }
 
     @Override
@@ -336,6 +309,33 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
         }
     }
 
+    private void analysisHrStatistics() {
+        if(device.getHrStatistics() != null) {
+            int[] hrHistogram = Arrays.copyOf(device.getHrStatistics(), device.getHrStatistics().length);
+            if(rlHrStatistics.getVisibility() == View.INVISIBLE) {
+                updateHrHistogram(hrHistogram);
+                rlHrStatistics.setVisibility(View.VISIBLE);
+                flEcgView.setVisibility(View.INVISIBLE);
+            }
+            else {
+                rlHrStatistics.setVisibility(View.INVISIBLE);
+                flEcgView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void updateHrHistogram(int[] hrHistData) {
+        hrHistogram.update(hrHistData);
+        int sum = 0;
+
+        if(hrHistData != null) {
+            for (int num : hrHistData) {
+                sum += num;
+            }
+        }
+        tvTotalBeatTimes.setText(String.valueOf(sum));
+    }
+
     private void initHrWarnAudioTrack() {
         int length = 4000;
         int fs = 1000;
@@ -351,157 +351,5 @@ public class EcgMonitorFragment extends BleDeviceFragment implements IEcgMonitor
         audioTrack.write(wave, 0, wave.length);
     }
 
-    private void hrStatistics() {
-        if(device.getHrStatistics() != null) {
-            int[] hrHistogram = Arrays.copyOf(device.getHrStatistics(), device.getHrStatistics().length);
-            if(rlHrStatistics.getVisibility() == View.INVISIBLE) {
-                updateBarChart(hrHistogram);
-                rlHrStatistics.setVisibility(View.VISIBLE);
-                flEcgView.setVisibility(View.INVISIBLE);
-            }
-            else {
-                rlHrStatistics.setVisibility(View.INVISIBLE);
-                flEcgView.setVisibility(View.VISIBLE);
-            }
-        }
-    }
 
-    private class StringAxisValueFormatter implements IAxisValueFormatter {
-        private List<String> mStrs;
-        /**
-     * 对字符串类型的坐标轴标记进行格式化
-     * @param strs
-     */
-        public StringAxisValueFormatter(List<String> strs){
-            this.mStrs =strs;
-        }
-
-        @Override
-        public String getFormattedValue(float value, AxisBase axisBase) {
-            int index = (int) value;
-            if (index < 0 || index >= mStrs.size()) {
-                return "";
-            } else {
-                return mStrs.get(index);
-            }
-        }
-    }
-
-    private void initBarChart(BarChart barChart) {
-        /***图表设置***/
-        //背景颜色
-        barChart.setBackgroundColor(Color.WHITE);
-        //不显示图表网格
-        barChart.setDrawGridBackground(false);
-        //背景阴影
-        barChart.setDrawBarShadow(false);
-        barChart.setHighlightFullBarEnabled(false);
-        //显示边框
-        barChart.setDrawBorders(true);
-        //设置动画效果
-        barChart.animateY(1000, Easing.Linear);
-        barChart.animateX(1000, Easing.Linear);
-
-        /***XY轴的设置***/
-        //X轴设置显示位置在底部
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        //xAxis.setAxisMinimum(0f);
-        xAxis.setGranularity(1f);
-        xAxis.setDrawAxisLine(false);
-        xAxis.setValueFormatter(new StringAxisValueFormatter(hrBarXStrings));
-
-        YAxis leftAxis = barChart.getAxisLeft();
-        YAxis rightAxis = barChart.getAxisRight();
-        //保证Y轴从0开始，不然会上移一点
-        leftAxis.setAxisMinimum(0f);
-        rightAxis.setAxisMinimum(0f);
-        leftAxis.setDrawAxisLine(false);
-        rightAxis.setDrawAxisLine(false);
-
-        /***折线图例 标签 设置***/
-        Legend legend = barChart.getLegend();
-        legend.setForm(Legend.LegendForm.LINE);
-        legend.setTextSize(11f);
-        //显示位置
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        //是否绘制在图表里面
-        legend.setDrawInside(false);
-
-        Description description = new Description();
-        description.setEnabled(false);
-        barChart.setDescription(description);
-
-        barChart.setDrawValueAboveBar(true);
-    }
-
-    private void updateHrBarData(int[] hrData) {
-        hrBarXStrings.clear();
-        hrBarEntries.clear();
-        if(hrData == null || hrData.length < 4) {
-            hrBarXStrings.add("无有效心率");
-            hrBarEntries.add(new BarEntry(0, 0.0f));
-        } else {
-            int j = 0;
-            hrBarXStrings.add("30以下");
-            hrBarEntries.add(new BarEntry(j++, (float) (hrData[0] + hrData[1] + hrData[2])));
-            for (int i = 3; i < hrData.length - 1; i++) {
-                if (hrData[i] > 5) {
-                    String key = String.valueOf(i * 10) + '-' + String.valueOf((i + 1) * 10);
-                    hrBarXStrings.add(key);
-                    hrBarEntries.add(new BarEntry(j++, (float) hrData[i]));
-                }
-            }
-            hrBarXStrings.add("200以上");
-            hrBarEntries.add(new BarEntry(j, (float) hrData[hrData.length - 1]));
-        }
-    }
-
-    /**
-     * 柱状图始化设置 一个BarDataSet 代表一列柱状图
-     *
-     * @param barColor      柱状图颜色
-     */
-    private BarDataSet initBarDataSet(String legendString, int barColor, int dataColor) {
-        // 每一个BarDataSet代表一类柱状图
-        BarDataSet barDataSet = new BarDataSet(hrBarEntries, legendString);
-
-        barDataSet.setColor(barColor);
-        barDataSet.setFormLineWidth(1f);
-        barDataSet.setFormSize(15.f);
-        //不显示柱状图顶部值
-        barDataSet.setDrawValues(true);
-        barDataSet.setValueTextSize(14f);
-        barDataSet.setValueTextColor(dataColor);
-        barDataSet.setValueFormatter(new IValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
-                return String.valueOf((int) value);
-            }
-        });
-        return barDataSet;
-    }
-
-    public void showBarChart() {
-        BarData data = new BarData(hrBarDateSet);
-        hrBarHistogram.setData(data);
-        hrBarHistogram.invalidate();
-
-    }
-
-    public void updateBarChart(int[] hrHistogram) {
-        updateHrBarData(hrHistogram);
-        hrBarDateSet.setValues(hrBarEntries);
-        showBarChart();
-        int sum = 0;
-
-        if(hrHistogram != null) {
-            for (int num : hrHistogram) {
-                sum += num;
-            }
-        }
-        tvHrTotal.setText(String.valueOf(sum));
-    }
 }
---
+++ app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/model/EcgHrHistogram.java
@@ -0,0 +1,184 @@
+package com.cmtech.android.bledevice.ecgmonitor.model;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * EcgHrHistogram: 心率直方图
+ * Created by bme on 2019/1/9.
+ */
+
+public class EcgHrHistogram extends BarChart {
+    private BarDataSet hrBarDateSet;
+    private List<BarEntry> hrBarEntries = new ArrayList<>();
+    private List<String> hrBarXStrings = new ArrayList<>();
+
+    public EcgHrHistogram(Context context) {
+        super(context);
+        initialize();
+    }
+
+    public EcgHrHistogram(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize();
+    }
+
+    public EcgHrHistogram(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initialize();
+    }
+
+    // 更新
+    public void update(int[] hrHistogram) {
+        updateHrBarData(hrHistogram);
+        hrBarDateSet.setValues(hrBarEntries);
+        invalidate();
+    }
+
+    @Override
+    public void invalidate() {
+        BarData data = new BarData(hrBarDateSet);
+        setData(data);
+        super.invalidate();
+    }
+
+
+    private void initialize() {
+        /***图表设置***/
+        //背景颜色
+        setBackgroundColor(Color.WHITE);
+        //不显示图表网格
+        setDrawGridBackground(false);
+        //背景阴影
+        setDrawBarShadow(false);
+        setHighlightFullBarEnabled(false);
+        //显示边框
+        setDrawBorders(true);
+        //设置动画效果
+        animateY(1000, Easing.Linear);
+        animateX(1000, Easing.Linear);
+
+        /***XY轴的设置***/
+        //X轴设置显示位置在底部
+        XAxis xAxis = getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(1f);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setValueFormatter(new StringAxisValueFormatter(hrBarXStrings));
+
+        YAxis leftAxis = getAxisLeft();
+        YAxis rightAxis = getAxisRight();
+        //保证Y轴从0开始，不然会上移一点
+        leftAxis.setAxisMinimum(0f);
+        rightAxis.setAxisMinimum(0f);
+        leftAxis.setDrawAxisLine(false);
+        rightAxis.setDrawAxisLine(false);
+
+        /***折线图例 标签 设置***/
+        Legend legend = getLegend();
+        legend.setForm(Legend.LegendForm.LINE);
+        legend.setTextSize(11f);
+        //显示位置
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        //是否绘制在图表里面
+        legend.setDrawInside(false);
+
+        Description description = new Description();
+        description.setEnabled(false);
+        setDescription(description);
+
+        setDrawValueAboveBar(true);
+
+        //updateHrBarData(null);
+
+        initBarDataSet("心率值统计", Color.BLUE, Color.BLACK);
+    }
+
+    /**
+     * 柱状图始化设置 一个BarDataSet 代表一列柱状图
+     *
+     * @param barColor      柱状图颜色
+     */
+    private void initBarDataSet(String legendString, int barColor, int dataColor) {
+        hrBarDateSet = new BarDataSet(hrBarEntries, legendString);
+
+        hrBarDateSet.setColor(barColor);
+        hrBarDateSet.setFormLineWidth(1f);
+        hrBarDateSet.setFormSize(15.f);
+        hrBarDateSet.setDrawValues(true);
+        hrBarDateSet.setValueTextSize(14f);
+        hrBarDateSet.setValueTextColor(dataColor);
+        hrBarDateSet.setValueFormatter(new IValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+                return String.valueOf((int) value);
+            }
+        });
+    }
+
+    private void updateHrBarData(int[] hrData) {
+        hrBarXStrings.clear();
+        hrBarEntries.clear();
+        if(hrData == null || hrData.length < 4) {
+            hrBarXStrings.add("无有效心率");
+            hrBarEntries.add(new BarEntry(0, 0.0f));
+        } else {
+            int j = 0;
+            hrBarXStrings.add("30以下");
+            hrBarEntries.add(new BarEntry(j++, (float) (hrData[0] + hrData[1] + hrData[2])));
+            for (int i = 3; i < hrData.length - 1; i++) {
+                if (hrData[i] > 5) {
+                    String key = String.valueOf(i * 10) + '-' + String.valueOf((i + 1) * 10);
+                    hrBarXStrings.add(key);
+                    hrBarEntries.add(new BarEntry(j++, (float) hrData[i]));
+                }
+            }
+            hrBarXStrings.add("200以上");
+            hrBarEntries.add(new BarEntry(j, (float) hrData[hrData.length - 1]));
+        }
+    }
+
+
+    private class StringAxisValueFormatter implements IAxisValueFormatter {
+        private List<String> mStrs;
+        /**
+             * 对字符串类型的坐标轴标记进行格式化
+             * @param strs
+             */
+        StringAxisValueFormatter(List<String> strs){
+            this.mStrs =strs;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axisBase) {
+            int index = (int) value;
+            if (index < 0 || index >= mStrs.size()) {
+                return "";
+            } else {
+                return mStrs.get(index);
+            }
+        }
+    }
+}
