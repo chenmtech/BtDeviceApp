41939b2a0d17ae6f13573c814fb01f815bb49fba
chenm-tongfang <chenm91@163.com>
2019-07-30 16:09:00 +0800

    commit;

--- app/src/main/java/com/cmtech/android/bledevice/siggenerator/model/SigGeneratorDevice.java
+++ app/src/main/java/com/cmtech/android/bledevice/siggenerator/model/SigGeneratorDevice.java
@@ -2,8 +2,35 @@ package com.cmtech.android.bledevice.siggenerator.model;
 
 import com.cmtech.android.ble.extend.BleDevice;
 import com.cmtech.android.ble.extend.BleDeviceBasicInfo;
+import com.cmtech.android.ble.extend.BleGattElement;
+import com.vise.log.ViseLog;
+
+import static com.cmtech.android.bledeviceapp.BleDeviceConstant.MY_BASE_UUID;
 
 public class SigGeneratorDevice extends BleDevice {
+    // 信号发生器Service UUID常量
+    private static final String sigGeneratorServiceUuid       = "aa50";           // 信号发生器服务UUID:aa50
+    private static final String sigGeneratorCtrlUuid          = "aa51";           // 启动/停止信号的控制UUID:aa51
+    private static final String sigGeneratorTypeUuid          = "aa52";           // 信号类型UUID:aa52
+    private static final String sigGeneratorMagUuid          = "aa53";           // 信号幅度UUID:aa53
+    private static final String sigGeneratorFreqUuid        = "aa54";           // 信号频率UUID:aa54
+    private static final String sigGeneratorBaselineUuid      = "aa55";           // 信号基线UUID:aa55
+
+    // Gatt Element常量
+    private static final BleGattElement SIGGENERATOR_CTRL =
+            new BleGattElement(sigGeneratorServiceUuid, sigGeneratorCtrlUuid, null, MY_BASE_UUID, "信号控制");
+    private static final BleGattElement SIGGENERATOR_TYPE =
+            new BleGattElement(sigGeneratorServiceUuid, sigGeneratorTypeUuid, null, MY_BASE_UUID, "信号类型");
+    private static final BleGattElement SIGGENERATOR_MAG =
+            new BleGattElement(sigGeneratorServiceUuid, sigGeneratorMagUuid, null, MY_BASE_UUID, "信号幅度");
+    private static final BleGattElement SIGGENERATOR_FREQ =
+            new BleGattElement(sigGeneratorServiceUuid, sigGeneratorFreqUuid, null, MY_BASE_UUID, "信号频率");
+    private static final BleGattElement SIGGENERATOR_BASELINE =
+            new BleGattElement(sigGeneratorServiceUuid, sigGeneratorBaselineUuid, null, MY_BASE_UUID, "信号基线");
+
+    // ECGMONITOR_CTRL Element的控制常量
+    private static final byte SIGGENERATOR_CTRL_STOP =             (byte) 0x00;        // 停止信号
+    private static final byte SIGGENERATOR_CTRL_START =             (byte) 0x01;        // 启动信号
 
     // 构造器
     public SigGeneratorDevice(BleDeviceBasicInfo basicInfo) {
@@ -13,7 +40,15 @@ public class SigGeneratorDevice extends BleDevice {
 
     @Override
     protected void executeAfterConnectSuccess() {
+        BleGattElement[] elements = new BleGattElement[]{SIGGENERATOR_CTRL, SIGGENERATOR_TYPE, SIGGENERATOR_MAG, SIGGENERATOR_FREQ, SIGGENERATOR_BASELINE};
 
+        if(!isContainGattElements(elements)) {
+            ViseLog.e("Signal Generator Elements are wrong.");
+
+            disconnect(true);
+
+            return;
+        }
     }
 
     @Override
@@ -24,5 +59,17 @@ public class SigGeneratorDevice extends BleDevice {
     @Override
     protected void executeAfterDisconnect() {
 
+    }
+
+    private void setSignalParameter() {
+
+    }
+
+    private void startGeneratingSignal() {
+        write(SIGGENERATOR_CTRL, SIGGENERATOR_CTRL_START, null);
+    }
+
+    private void stopGeneratingSignal() {
+        write(SIGGENERATOR_CTRL, SIGGENERATOR_CTRL_STOP, null);
     }
 }
