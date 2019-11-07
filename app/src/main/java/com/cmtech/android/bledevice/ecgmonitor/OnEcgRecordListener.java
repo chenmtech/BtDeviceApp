9f6c43ac695fc6416435a99d5068ed47bf4f0c3a
chenm-tongfang <chenm91@163.com>
2019-11-08 00:37:05 +0800

    commit;

--- app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/OnEcgRecordListener.java
+++ app/src/main/java/com/cmtech/android/bledevice/ecgmonitor/OnEcgRecordListener.java
@@ -1,17 +1,17 @@
 package com.cmtech.android.bledevice.ecgmonitor;
 
 public interface OnEcgRecordListener {
-    void onRecordCreated();
-    void onRecordNameSetup(String recordName);
-    void onCreateTimeUpdated(long createTime);
-    void onCreatorUpdated(String creatorId);
-    void onModifyTimeUpdated(long modifyTime);
-    void onSampleRateUpdated(int sampleRate);
-    void onCaliValueUpdated(int caliValue);
-    void onDeviceAddressUpdated(String address);
-    void onLeadTypeUpdated(int leadTypeCode);
-    void onSignalUpdated(int ecgSignal);
-    void onHrValueUpdated(short hr);
-    void onStart();
-    void onStop();
+    void onRecordCreated(); // 创建新记录
+    void onRecordNameSetup(String recordName); // 设置记录名，unique
+    void onCreateTimeUpdated(long createTime); // 更新创建时间
+    void onCreatorUpdated(String creatorId); // 更新创建人
+    void onModifyTimeUpdated(long modifyTime); // 更新修改时间
+    void onSampleRateUpdated(int sampleRate); // 更新采样率
+    void onCaliValueUpdated(int caliValue); // 更新标定值
+    void onDeviceAddressUpdated(String address); // 更新设备地址
+    void onLeadTypeUpdated(int leadTypeCode); // 更新导联类型
+    void onSignalUpdated(int ecgSignal); // 更新信号值
+    void onHrValueUpdated(short hr); // 更新心率值
+    void onStart(); // 开始记录
+    void onStop(); // 停止记录
 }
