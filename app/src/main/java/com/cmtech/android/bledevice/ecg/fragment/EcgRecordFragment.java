7daebc608d9417cd9e932d29df995ad6898d1cea
chenmtech <1276094137@qq.com>
2019-12-06 10:58:07 +0800

    commit;

--- app/src/main/java/com/cmtech/android/bledevice/ecg/fragment/EcgRecordFragment.java
+++ app/src/main/java/com/cmtech/android/bledevice/ecg/fragment/EcgRecordFragment.java
@@ -5,8 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,11 +62,9 @@ public class EcgRecordFragment extends Fragment{
         setSignalSecNum(device.getRecordSecond());
 
         rvMarker = view.findViewById(R.id.rv_ecg_marker);
-        LinearLayoutManager markerLayoutManager = new LinearLayoutManager(getContext());
-        markerLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        rvMarker.setLayoutManager(markerLayoutManager);
-        if(getContext() != null)
-            rvMarker.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        GridLayoutManager layoutManage = new GridLayoutManager(getContext(), 2);
+        rvMarker.setLayoutManager(layoutManage);
+        rvMarker.setLayoutManager(layoutManage);
         List<EcgAbnormal> ecgAbnormals = new ArrayList<>();
         if(device.isLocal()) {
             for(int i = 0; i < 4; i++)
