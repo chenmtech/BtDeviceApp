0490bfc8d54da6bb4190871ddc23af770d1f8488
chenm-tongfang <chenm91@163.com>
2019-12-14 06:54:43 +0800

    commit;

--- app/src/main/java/com/cmtech/android/bledevice/ecg/webecg/WebEcgDevice.java
+++ app/src/main/java/com/cmtech/android/bledevice/ecg/webecg/WebEcgDevice.java
@@ -176,6 +176,7 @@ public class WebEcgDevice extends AbstractEcgDevice {
             dataCache.clear();
         }
         handler.removeCallbacksAndMessages(null);
+        updateSignalShowState(false);
         super.forceDisconnect(forever);
     }
 
