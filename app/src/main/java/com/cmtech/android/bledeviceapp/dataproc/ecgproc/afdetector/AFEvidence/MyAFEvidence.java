0b71af742c3b366f0abf9f995d766c7de2a48796
chenmtech <1276094137@qq.com>
2020-11-16 15:34:51 +0800

    优化房颤检测算法；

--- app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/afdetector/AFEvidence/MyAFEvidence.java
+++ app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/afdetector/AFEvidence/MyAFEvidence.java
@@ -5,26 +5,28 @@ import com.cmtech.android.bledeviceapp.dataproc.ecgproc.afdetector.IAFDetector;
 import java.util.List;
 
 public class MyAFEvidence implements IAFDetector {
-	private static final int MIN_MS = -600;
-	private static final int MAX_MS = 600;
-	private static final int BIN_SIZE = 40;
-	private static final int MAX_BIN = (MAX_MS - MIN_MS)/BIN_SIZE-1;
+    private static final int MIN_MS = -600;
+    private static final int MAX_MS = 600;
+    private static final int BIN_SIZE = 40;
+    private static final int MAX_BIN = (MAX_MS - MIN_MS)/BIN_SIZE-1;
 
-	private static final int THRESHOLD = 10;
+    private static final int MIN_RR_NUM = 10;
+
+    private static final int THRESHOLD = 7;
 
 
-	private int classifyResult = UNDETERMIN;
-	private int afe = 0;
+    private int classifyResult = UNDETERMIN;
+    private int afe = 0;
 
-    private int OriginCount = 0;
+    private int originCount = 0;
     private final MyHistogram hist;
 
     public MyAFEvidence() {
         hist = new MyHistogram();
     }
 
-	public void process(List<Double> RR) {
-        if(RR == null || RR.size() < 10) {
+    public void process(List<Double> RR) {
+        if(RR == null || RR.size() < MIN_RR_NUM) {
             afe = 0;
             classifyResult = UNDETERMIN;
         } else {
@@ -40,62 +42,69 @@ public class MyAFEvidence implements IAFDetector {
             }
         }
         clear();
-	}
+    }
 
-	public int getAFEvidence() {
+    public int getAFEvidence() {
         return afe;
     }
 
     @Override
-	public int getClassifyResult() {
+    public int getClassifyResult() {
         return classifyResult;
     }
-	
-	private void clear() {
-		OriginCount = 0;
-		hist.clear();
-	}
-	
-	private void addPoint(double xMs, double yMs) {
-		// 在原点，OriginCount计数
-		if(Math.abs(xMs) < 20 && Math.abs(yMs) < 20)
-			OriginCount++;
-		
-		// 异常点，丢弃
-		if(isOutLier(xMs, yMs)) return;
-		
-		// 添加到直方图
-		xMs = (xMs - MIN_MS) / BIN_SIZE;
-		int x;
-		if(xMs < 0)
-			x = 0;
-		else if(xMs > MAX_BIN) {
-			x = MAX_BIN;
-		} else {
-			x = (int)xMs;
-		}
-		
-		yMs = (yMs - MIN_MS) / BIN_SIZE;
-		int y;
-		if(yMs < 0)
-			y = 0;
-		else if(yMs > MAX_BIN) {
-			y = MAX_BIN;
-		} else {
-			y = (int)yMs;
-		}
 
-		hist.addPoint(x, y);
-		
-	}
-	
-	private int calculateAFEvidence() {
-		return hist.getIrregularityEvidence() - OriginCount -2*hist.getPACEvidence();
-	}
-	
-	private boolean isOutLier(double xMs, double yMs) {
-		return (xMs >= 1500 || yMs >= 1500);
-	}
-	
-	
+    private void clear() {
+        originCount = 0;
+        hist.clear();
+    }
+
+    private void addPoint(double xMs, double yMs) {
+        // 异常点，丢弃
+        if(isOutLier(xMs, yMs)) return;
+
+        // 在原点，originCount计数
+        if(Math.abs(xMs) < 20 && Math.abs(yMs) < 20)
+            originCount++;
+
+        // 添加到直方图
+        xMs = (xMs - MIN_MS) / BIN_SIZE;
+        int x;
+        if(xMs < 0)
+            x = 0;
+        else if(xMs > MAX_BIN) {
+            x = MAX_BIN;
+        } else {
+            x = (int)xMs;
+        }
+
+        yMs = (yMs - MIN_MS) / BIN_SIZE;
+        int y;
+        if(yMs < 0)
+            y = 0;
+        else if(yMs > MAX_BIN) {
+            y = MAX_BIN;
+        } else {
+            y = (int)yMs;
+        }
+
+        hist.addPoint(x, y);
+
+    }
+
+    private int calculateAFEvidence() {
+        return hist.getIrregularityEvidence() - originCount -2*hist.getPACEvidence();
+    }
+
+    private boolean isOutLier(double xMs, double yMs) {
+        return (xMs >= 1500 || yMs >= 1500);
+    }
+
+    public void printHistogram() {
+        for(int i = 0; i <= 29; i++) {
+            for(int j = 0; j <= 29; j++) {
+                System.out.printf("%7s", hist.getBelongSegLabel(i, j));
+            }
+            System.out.println();
+        }
+    }
 }
