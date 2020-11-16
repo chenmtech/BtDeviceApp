82166eca76931dad8a1a46c447deef9b271c97cd
chenmtech <1276094137@qq.com>
2020-11-16 11:53:45 +0800

    修正R波检测bug;

--- app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/preproc/EcgPreProcessor.java
+++ app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/preproc/EcgPreProcessor.java
@@ -8,7 +8,6 @@ import com.cmtech.dsp.filter.design.FIRDesigner;
 import com.cmtech.dsp.filter.design.FilterType;
 import com.cmtech.dsp.filter.design.NotchDesigner;
 import com.cmtech.dsp.filter.design.WinType;
-import com.vise.log.ViseLog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,28 +15,28 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import static com.cmtech.android.bledeviceapp.global.AppConstant.INVALID_HR;
-
 public class EcgPreProcessor {
-	private static final int NUM_BEFORE_R = 99;
-	private static final int NUM_AFTER_R = 150;
+    private static final int NUM_BEFORE_R = 99;
+    private static final int NUM_AFTER_R = 150;
+    private static final int MIN_QRS_NUM = 6;
 
-	private int sampleRate;
-	private Map<String, Object> processResult = new HashMap<>();
-	
-	public EcgPreProcessor() {
-	}
-	
-	public Map<String, Object> getProcessResult() {
-		return processResult;
-	}
-	
-	public JSONObject getResultJson() {
-	    try {
+    private static final int INVALID_HR = 0;
+
+    private int sampleRate;
+    private Map<String, Object> processResult = new HashMap<>();
+
+    public EcgPreProcessor() {
+    }
+
+    public Map<String, Object> getProcessResult() {
+        return processResult;
+    }
+
+    public JSONObject getResultJson() {
+        try {
             if (processResult.isEmpty()) return null;
             JSONObject json = new JSONObject();
             for (Entry<String, Object> ent : processResult.entrySet()) {
@@ -45,27 +44,27 @@ public class EcgPreProcessor {
             }
             return json;
         } catch (JSONException ex) {
-	        ex.printStackTrace();
+            ex.printStackTrace();
         }
-		return null;
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Double> getRRIntervalInMs() {
-		if(processResult.isEmpty()) return null;
-		List<Long> RPos = (List<Long>) processResult.get("RPos");
-		int sampleRate = (int) processResult.get("SampleRate");
-		List<Double> RRList = new ArrayList<>();
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Double> getRRIntervalInMs() {
+        if(processResult.isEmpty()) return null;
+        List<Long> RPos = (List<Long>) processResult.get("RPos");
+        int sampleRate = (int) processResult.get("SampleRate");
+        List<Double> RRList = new ArrayList<>();
         for(int i = 1; i < RPos.size(); i++) {
             double rr = (RPos.get(i)-RPos.get(i-1))*1000.0/sampleRate;
-        	RRList.add(rr);
+            RRList.add(rr);
         }
         return RRList;
-	}
+    }
 
-	public int getAverageHr() {
-	    List<Double> RR = getRRIntervalInMs();
-	    if(RR == null) return INVALID_HR;
+    public int getAverageHr() {
+        List<Double> RR = getRRIntervalInMs();
+        if(RR == null) return INVALID_HR;
         double aveRR = MathUtil.doubleAve(RR);
         return (int)Math.round(60000/aveRR);
     }
@@ -76,187 +75,186 @@ public class EcgPreProcessor {
         return MathUtil.doubleStd(RR);
     }
 
-	@SuppressWarnings("unchecked")
-	public void process(List<Short> ecgData, int sampleRate) {
-	    processResult.clear();
+    @SuppressWarnings("unchecked")
+    public void process(List<Short> ecgData, int sampleRate) {
+        processResult.clear();
 
-		if(ecgData == null || ecgData.isEmpty()) {
-			return;
-		}
+        if(ecgData == null || ecgData.isEmpty()) {
+            return;
+        }
 
-		this.sampleRate = sampleRate;
-		
-		// 滤波
-		//IIRFilter dcBlocker = DCBlockDesigner.design(1, sampleRate);
-		IIRFilter notch50 = designNotch(50);
+        this.sampleRate = sampleRate;
+
+        // 滤波
+        //IIRFilter dcBlocker = DCBlockDesigner.design(1, sampleRate);
+/*		IIRFilter notch50 = designNotch(50);
 		FIRFilter lpFilter = designLpFilter();
 		List<Short> afterFilter = new ArrayList<Short>();
 		for(Short d : ecgData) {
 			afterFilter.add((short)Math.round(lpFilter.filter(notch50.filter(d))));
 		}
-		ecgData = afterFilter;
-		
-		// 检测QRS波和RR间隔
-		Map<String, Object> qrsAndRRInterval = getQrsPosAndRRInterval(ecgData);
-		if(qrsAndRRInterval == null || qrsAndRRInterval.isEmpty()) {
-		    return;
-        }
-        ViseLog.e(qrsAndRRInterval);
-		
-		// 检测R波和每次心跳开始位置
-		Map<String, Object> rPosAndBeatBegin = getRWaveAndBeatBeginPos(ecgData, qrsAndRRInterval);
-        ViseLog.e(rPosAndBeatBegin);
+		ecgData = afterFilter;*/
 
-		// 对ECG分割为单次心跳数据，并归一化
-		List<Long> beatBegin = (List<Long>)rPosAndBeatBegin.get("BeatBegin");
-		List<Float> normalizedEcgData = normalizeEcgData(ecgData, beatBegin);
-		List<Long> rPos =  (List<Long>)rPosAndBeatBegin.get("RPos");
+        // 检测QRS波和RR间隔
+        Map<String, Object> qrsAndRRInterval = getQrsPosAndRRInterval(ecgData);
+        if(qrsAndRRInterval == null || qrsAndRRInterval.isEmpty()) {
+            return;
+        }
+
+        // 检测R波和每次心跳开始位置
+        Map<String, Object> rPosAndBeatBegin = getRWaveAndBeatBeginPos(ecgData, qrsAndRRInterval);
+
+        // 对ECG分割为单次心跳数据，并归一化
+        List<Long> beatBegin = (List<Long>)rPosAndBeatBegin.get("BeatBegin");
+        List<Float> normalizedEcgData = normalizeEcgData(ecgData, beatBegin);
+        List<Long> rPos =  (List<Long>)rPosAndBeatBegin.get("RPos");
         List<List<Float>> segEcgData = segmentEcgData(normalizedEcgData, rPos, beatBegin);
 
-		// 打包处理结果
-		processResult = rPosAndBeatBegin;
-		processResult.put("QrsPos", qrsAndRRInterval.get("QrsPos"));
-		processResult.put("EcgData", ecgData);
-		processResult.put("SegEcgData", segEcgData);
-		processResult.put("SampleRate", sampleRate);
-	}	
-	
-	public String getSegEcgDataString() {
-		if(processResult.isEmpty()) return "";
+        // 打包处理结果
+        processResult = rPosAndBeatBegin;
+        processResult.put("QrsPos", qrsAndRRInterval.get("QrsPos"));
+        processResult.put("EcgData", ecgData);
+        processResult.put("SegEcgData", segEcgData);
+        processResult.put("SampleRate", sampleRate);
+    }
+
+    public String getSegEcgDataString() {
+        if(processResult.isEmpty()) return "";
         List<List<Float>> segEcgData = (List<List<Float>>) processResult.get("SegEcgData");
-		if(segEcgData == null || segEcgData.isEmpty()) return "";
-		
-		StringBuilder builder = new StringBuilder();
-		
-		for(List<Float> seg : segEcgData) {
-			for(Float d : seg) {
-				builder.append(String.format(Locale.getDefault(), "%.3f", d));
-				builder.append(' ');
-			}
-			builder.append("\r\n");
-		}
-		return builder.toString();
-	}
-	
-	private static List<List<Float>> segmentEcgData(List<Float> normalizedEcgData, List<Long> rPos, List<Long> beatBeginPos) {
-		List<List<Float>> segEcgData = new ArrayList<>();
-		
-		for(int i = 0; i < beatBeginPos.size()-1; i++) {
-			List<Float> oneBeat = new ArrayList<>();
-			
-			long begin = beatBeginPos.get(i);
-			long end = beatBeginPos.get(i+1);
-			long r = rPos.get(i);
-			long fillBefore = 0;
-			long fillAfter = 0;
-			float first = 0.0f;
-			float last = 0.0f;
-			if(r - begin >= NUM_BEFORE_R) {
-				begin = r - NUM_BEFORE_R;
-			} else {
-				fillBefore = NUM_BEFORE_R - (r-begin);
-				first = normalizedEcgData.get((int)begin);
-			}
-			if(end-r > NUM_AFTER_R) {
-				end = r + NUM_AFTER_R + 1;
-			} else {
-				fillAfter = NUM_AFTER_R - (end-r) + 1;
-				last = normalizedEcgData.get((int)(end-1));
-			}
-			for(int j = 0; j < fillBefore; j++) {
-				oneBeat.add(first);
-			}
-			for(long pos = begin; pos < end; pos++) {
-				oneBeat.add(normalizedEcgData.get((int)pos));
-			}
-			for(int j = 0; j < fillAfter; j++) {
-				oneBeat.add(last);
-			}
-			segEcgData.add(oneBeat);
-			//System.out.println(oneBeat.size());
-		}
-		return segEcgData;
-	}
-	
-	private Map<String, Object> getQrsPosAndRRInterval(List<Short> ecgData) {
-		QrsDetectorWithQRSInfo qrsDetector = new QrsDetectorWithQRSInfo(sampleRate);
-		int n = 0;
-		for(Short datum : ecgData) {
-			qrsDetector.outputRRInterval((int)datum);
-			n++;
-			if(qrsDetector.firstPeakFound()) break;
-		}
-		for(Short datum : ecgData) {
-			qrsDetector.outputRRInterval((int)datum);
-		}
-		
-		List<Long> qrsPos = qrsDetector.getQrsPositions();
-		List<Integer> rrInterval = qrsDetector.getRRIntervals();
+        if(segEcgData == null || segEcgData.isEmpty()) return "";
 
-		if(qrsPos.size() < 6)
-		    return null;
+        StringBuilder builder = new StringBuilder();
 
-		qrsPos.remove(0);
-		rrInterval.remove(0);
-		for(int i = 0; i < qrsPos.size(); i++) {
-			long p = qrsPos.get(i)-n;
-			if(p < 0) p = 0;
-			qrsPos.set(i, p);
-		}
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("QrsPos", qrsPos);
-		map.put("RRInterval", rrInterval);		
-		
-		return map;
-	}
-	
-	private static Map<String, Object> getRWaveAndBeatBeginPos(List<Short> ecgData, Map<String, Object> qrsAndRRInterval) {
-		return RWaveDetecter.findRWaveAndBeatBeginPos(ecgData, qrsAndRRInterval);
-	}
-	
-	private static List<Float> normalizeEcgData(List<Short> ecgData, List<Long> beatBeginPos) {
-		List<Float> normalized = new ArrayList<>();
-		
-		for(Short d : ecgData) {
-			normalized.add((float)d);
-		}
-		
-		List<Float> oneBeat =  new ArrayList<>();
-		for(int i = 0; i < beatBeginPos.size()-1; i++) {
-			for(long pos = beatBeginPos.get(i); pos < beatBeginPos.get(i+1); pos++) {
-				oneBeat.add(normalized.get((int)pos));
-			}
-			
-			float ave = MathUtil.floatAve(oneBeat);
-			float std = MathUtil.floatStd(oneBeat);
-			
-			for(int ii = 0; ii < oneBeat.size(); ii++) {
-				oneBeat.set(ii, (oneBeat.get(ii)-ave)/std);
-			}
-			
-			for(long pos = beatBeginPos.get(i),  ii = 0; pos < beatBeginPos.get(i+1); pos++, ii++) {
-				normalized.set((int)pos, oneBeat.get((int)ii));
-			}
-			
-			oneBeat.clear();
-		}
-		return normalized;
-	}
+        for(List<Float> seg : segEcgData) {
+            for(Float d : seg) {
+                builder.append(String.format("%.3f", d));
+                builder.append(' ');
+            }
+            builder.append("\r\n");
+        }
+        return builder.toString();
+    }
 
-	private FIRFilter designLpFilter() {
-		double[] wp = {2*Math.PI*65/sampleRate};
-		double[] ws = {2*Math.PI*85/sampleRate};
-		double Rp = 1;
-		double As = 50;
-		FilterType fType = FilterType.LOWPASS;
-		WinType wType = WinType.HAMMING;
+    private static List<List<Float>> segmentEcgData(List<Float> normalizedEcgData, List<Long> rPos, List<Long> beatBeginPos) {
+        List<List<Float>> segEcgData = new ArrayList<>();
+
+        for(int i = 0; i < beatBeginPos.size()-1; i++) {
+            List<Float> oneBeat = new ArrayList<>();
+
+            long begin = beatBeginPos.get(i);
+            long end = beatBeginPos.get(i+1);
+            long r = rPos.get(i);
+            long fillBefore = 0;
+            long fillAfter = 0;
+            float first = 0.0f;
+            float last = 0.0f;
+            if(r - begin >= NUM_BEFORE_R) {
+                begin = r - NUM_BEFORE_R;
+            } else {
+                fillBefore = NUM_BEFORE_R - (r-begin);
+                first = normalizedEcgData.get((int)begin);
+            }
+            if(end-r > NUM_AFTER_R) {
+                end = r + NUM_AFTER_R + 1;
+            } else {
+                fillAfter = NUM_AFTER_R - (end-r) + 1;
+                last = normalizedEcgData.get((int)(end-1));
+            }
+            for(int j = 0; j < fillBefore; j++) {
+                oneBeat.add(first);
+            }
+            for(long pos = begin; pos < end; pos++) {
+                oneBeat.add(normalizedEcgData.get((int)pos));
+            }
+            for(int j = 0; j < fillAfter; j++) {
+                oneBeat.add(last);
+            }
+            segEcgData.add(oneBeat);
+            //System.out.println(oneBeat.size());
+        }
+        return segEcgData;
+    }
+
+    private Map<String, Object> getQrsPosAndRRInterval(List<Short> ecgData) {
+        QrsDetectorWithQRSInfo qrsDetector = new QrsDetectorWithQRSInfo(sampleRate);
+        int n = 0;
+        for(Short datum : ecgData) {
+            qrsDetector.outputRRInterval((int)datum);
+            n++;
+            if(qrsDetector.firstPeakFound()) break;
+        }
+        for(Short datum : ecgData) {
+            qrsDetector.outputRRInterval((int)datum);
+        }
+        List<Long> qrsPos = qrsDetector.getQrsPositions();
+        if(qrsPos.size() < MIN_QRS_NUM) // 波形太少了
+            return null;
+        // 减掉n
+        qrsPos.remove(0);
+        for(int i = 0; i < qrsPos.size(); i++) {
+            long p = qrsPos.get(i)-n;
+            if(p < 0) p = 0;
+            qrsPos.set(i, p);
+        }
+
+        List<Integer> rrInterval = new ArrayList<>();
+        for(int i = 0; i < qrsPos.size()-1; i++) {
+            rrInterval.add((int) (qrsPos.get(i+1)-qrsPos.get(i)));
+        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("QrsPos", qrsPos);
+        map.put("RRInterval", rrInterval);
+
+        return map;
+    }
+
+    private Map<String, Object> getRWaveAndBeatBeginPos(List<Short> ecgData, Map<String, Object> qrsAndRRInterval) {
+        return RWaveDetecter.findRWaveAndBeatBeginPos(ecgData, qrsAndRRInterval, sampleRate);
+    }
+
+    private static List<Float> normalizeEcgData(List<Short> ecgData, List<Long> beatBeginPos) {
+        List<Float> normalized = new ArrayList<>();
+
+        for(Short d : ecgData) {
+            normalized.add((float)d);
+        }
+
+        List<Float> oneBeat =  new ArrayList<>();
+        for(int i = 0; i < beatBeginPos.size()-1; i++) {
+            for(long pos = beatBeginPos.get(i); pos < beatBeginPos.get(i+1); pos++) {
+                oneBeat.add(normalized.get((int)pos));
+            }
+
+            float ave = MathUtil.floatAve(oneBeat);
+            float std = MathUtil.floatStd(oneBeat);
+
+            for(int ii = 0; ii < oneBeat.size(); ii++) {
+                oneBeat.set(ii, (oneBeat.get(ii)-ave)/std);
+            }
+
+            for(long pos = beatBeginPos.get(i),  ii = 0; pos < beatBeginPos.get(i+1); pos++, ii++) {
+                normalized.set((int)pos, oneBeat.get((int)ii));
+            }
+
+            oneBeat.clear();
+        }
+        return normalized;
+    }
+
+    private FIRFilter designLpFilter() {
+        double[] wp = {2*Math.PI*65/sampleRate};
+        double[] ws = {2*Math.PI*85/sampleRate};
+        double Rp = 1;
+        double As = 50;
+        FilterType fType = FilterType.LOWPASS;
+        WinType wType = WinType.HAMMING;
 
         return FIRDesigner.design(wp, ws, Rp, As, fType, wType);
-	}
-	
-	private static IIRFilter designNotch(int f0) {
+    }
+
+    private static IIRFilter designNotch(int f0) {
         return NotchDesigner.design(f0, 2);
-	}
-	
+    }
+
 }
--- app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/preproc/RWaveDetecter.java
+++ app/src/main/java/com/cmtech/android/bledeviceapp/dataproc/ecgproc/preproc/RWaveDetecter.java
@@ -6,7 +6,6 @@ import com.cmtech.android.bledeviceapp.util.MathUtil;
 import com.cmtech.dsp.filter.FIRFilter;
 import com.cmtech.dsp.filter.IDigitalFilter;
 import com.cmtech.dsp.filter.structure.StructType;
-import com.vise.log.ViseLog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,104 +13,133 @@ import java.util.List;
 import java.util.Map;
 
 public class RWaveDetecter {
-	private static final int QRS_WIDTH_MS = 300; // unit:ms
-	private static final int SAMPLE_RATE = 360; // sample rate
-	private static final int QRS_WIDTH = QRS_WIDTH_MS*SAMPLE_RATE/1000;
-	private static final int QRS_HALF_WIDTH = QRS_WIDTH/2;
-	
-	@SuppressWarnings("unchecked")
-	public static Map<String, Object> findRWaveAndBeatBeginPos(List<Short> ecgData, Map<String, Object> qrsAndRRInterval) {
-		List<Long> qrsPos = (List<Long>) qrsAndRRInterval.get("QrsPos");
-		List<Integer> rrInterval = (List<Integer>) qrsAndRRInterval.get("RRInterval");
-		
-		List<Float> d2= diffFilter(ecgData);
-		List<Long> rPos = new ArrayList<>();
-		List<Float> normData = new ArrayList<>();
-		for(Short d : ecgData) {
-			normData.add((float)d);
-		}
-		for(int i = 1; i < rrInterval.size()-1; i++) {
-			long qrs = qrsPos.get(i+1);
-			long qrsBegin = qrs - QRS_HALF_WIDTH;
-			long qrsEnd = qrs + QRS_HALF_WIDTH;
-			long beatBegin = qrs - Math.round(rrInterval.get(i)*2.0/5);
-			long beatEnd = qrs + Math.round(rrInterval.get(i+1)*3.0/5);
-			
-			List<Short> temp = ecgData.subList((int)beatBegin, (int)beatEnd);
-			float ave = MathUtil.shortAve(temp);
-			float std = MathUtil.shortStd(temp);
-			
-			long ii = beatBegin;
-			for(Short d : temp) {
-				normData.set((int) ii, (d - ave)/std);
-				ii++;
-			}
-			
-			Pair<Integer, Float> rlt = MathUtil.floatMin(normData.subList((int)qrsBegin, (int)qrsEnd));
-			float minV = rlt.second;
-			int minI = rlt.first;
-			
-			rlt = MathUtil.floatMax(normData.subList((int)qrsBegin, (int)qrsEnd));
-			float maxV = rlt.second;
-			int maxI = rlt.first;
-			
-			if(Math.abs(maxV) > 2*Math.abs(minV)/3) {
-				rPos.add(qrsBegin + maxI);
-			} else {
-				rlt = MathUtil.floatMin(d2.subList((int)qrsBegin, (int)qrsEnd));
-				minV = rlt.second;
-				minI = rlt.first;
-				
-				rlt = MathUtil.floatMax(d2.subList((int)qrsBegin, (int)qrsEnd));
-				maxV = rlt.second;
-				maxI = rlt.first;
-				
-				if(Math.abs(maxV) > Math.abs(minV)) {
-					rPos.add(qrsBegin + maxI - 2);
-				} else {
-					rPos.add(qrsBegin + minI - 2);
-				}
-			}
-			
-			long rBegin = rPos.get(i-1) - 5;
-			long rEnd = rPos.get(i-1) + 5;
-			List<Float> tmp = new ArrayList<>();
-			for(long j = rBegin; j <= rEnd; j++) {
-				tmp.add((float)Math.abs(ecgData.get((int)j)));
-			}
-			
-			rlt = MathUtil.floatMax(tmp);
-			maxV = rlt.second;
-			maxI = rlt.first;
-			rPos.set(i-1, rBegin + maxI);			
-		}	
-		
-		List<Long> beatBegin = new ArrayList<>();
-		for(int i = 1; i < rrInterval.size()-1; i++) {
-			beatBegin.add(rPos.get(i-1) - Math.round(rrInterval.get(i)*2.0/5));
-		}
+    private static final int QRS_WIDTH_MS = 200; // QRS波宽度，unit:毫秒
+    private static final float MAX_PEAK_MIN_PEAK_RATIO = 1.0f/3; // 可接受的正峰值与负峰值之比
 
-		if(beatBegin.get(0) < 0) {
-            beatBegin.remove(0);
-            rPos.remove(0);
+    @SuppressWarnings("unchecked")
+    public static Map<String, Object> findRWaveAndBeatBeginPos(List<Short> ecgData, Map<String, Object> qrsAndRRInterval, int sampleRate) {
+        int halfQrsWidth = QRS_WIDTH_MS*sampleRate/1000/2; // QRS波一半宽度点数
+
+        List<Long> qrsPos = (List<Long>) qrsAndRRInterval.get("QrsPos"); // QRS波位置
+        List<Integer> rrInterval = (List<Integer>) qrsAndRRInterval.get("RRInterval"); // 初步的RR间隔
+        if(rrInterval.size() != qrsPos.size()-1) return null; //RR间隔的个数必须是QRS波个数-1
+
+        List<Float> delta2= diffFilter(ecgData); // 信号二阶差分
+
+        List<Long> rPos = new ArrayList<>(); // 存放R波位置
+
+        for(int i = 1; i < qrsPos.size()-1; i++) {
+            long qrs = qrsPos.get(i);
+
+            long beatBegin = qrs - Math.round(rrInterval.get(i-1)*2.0/5);
+            long qrsBegin = qrs - halfQrsWidth;
+            long minTmp = Math.min(beatBegin, qrsBegin);
+            if(minTmp < 0) continue;
+            beatBegin = minTmp;
+
+            long beatEnd = qrs + Math.round(rrInterval.get(i)*3.0/5);
+            long qrsEnd = qrs + halfQrsWidth;
+            long maxTmp = Math.max(beatEnd, qrsEnd);
+            if(maxTmp >= ecgData.size()) continue;
+            beatEnd = maxTmp;
+
+            // 截取当前一次心跳的数据
+            List<Float> beat = new ArrayList<>();
+            for(Short d : ecgData.subList((int)beatBegin, (int)beatEnd)) {
+                beat.add((float)d);
+            }
+
+            // 归一化处理
+            float ave = MathUtil.floatAve(beat);
+            float std = MathUtil.floatStd(beat);
+            for(int i1 = 0; i1 < beat.size(); i1++) {
+                beat.set(i1, (beat.get(i1)-ave)/std);
+            }
+
+            // 把beat缩小为QRS波的范围
+            beat = beat.subList((int)(qrsBegin-beatBegin), (int)(qrsEnd-beatBegin));
+
+            // 搜索beat的最小值和最大值
+            Pair<Integer, Float> rlt = MathUtil.floatMin(beat);
+            float minV = rlt.second;
+            int minI = rlt.first;
+
+            rlt = MathUtil.floatMax(beat);
+            float maxV = rlt.second;
+            int maxI = rlt.first;
+
+
+            if(maxI < minI && Math.abs(maxV) > MAX_PEAK_MIN_PEAK_RATIO*Math.abs(minV)) {
+                rPos.add(qrsBegin + maxI);
+                System.out.println("MaxV peak > "+ MAX_PEAK_MIN_PEAK_RATIO + " minV peak");
+            } else {
+                System.out.println("MaxV peak <= "+ MAX_PEAK_MIN_PEAK_RATIO + " minV peak");
+                List<Float> qrsDelta2 = delta2.subList((int)qrsBegin, (int)qrsEnd); // QRS波的二阶差分数据
+
+                rlt = MathUtil.floatMin(qrsDelta2);
+                minV = rlt.second;
+                minI = rlt.first;
+
+                rlt = MathUtil.floatMax(qrsDelta2);
+                maxV = rlt.second;
+                maxI = rlt.first;
+
+                long qrsPosTmp;
+                if(Math.abs(maxV) > Math.abs(minV)) {
+                    qrsPosTmp = qrsBegin + maxI;
+                } else {
+                    qrsPosTmp = qrsBegin + minI;
+                }
+
+                // 最后在qrsPosTmp附近搜索峰值
+                long rBegin = qrsPosTmp - 5;
+                long rEnd = qrsPosTmp + 5;
+                List<Float> tmpList = new ArrayList<>();
+                for(long j = rBegin; j <= rEnd; j++) {
+                    tmpList.add((float)Math.abs(ecgData.get((int)j)));
+                }
+
+                rlt = MathUtil.floatMax(tmpList);
+                rPos.add(rBegin + rlt.first);
+            }
         }
-		
-		Map<String, Object> map = new HashMap<>();
-		map.put("RPos", rPos);
-		map.put("BeatBegin", beatBegin);
-		return map;
-	}
-	
-	private static List<Float> diffFilter(List<Short> data) {
-		double[] b = {1,0,-2,0,1};
-		IDigitalFilter diffFilter = new FIRFilter(b);
-		diffFilter.createStructure(StructType.FIR_LPF);
-		
-		List<Float> d2Data = new ArrayList<>();
-		for(Short d : data) {
-			d2Data.add((float)diffFilter.filter(d));
-		}
-		
-		return d2Data;
-	}
+
+        // 求新的RR间隔
+        List<Integer> newRRInterval = new ArrayList<>();
+        for(int i = 1; i < rPos.size(); i++) {
+            newRRInterval.add((int)(rPos.get(i)-rPos.get(i-1)));
+        }
+
+        // 求每次心跳开始位置
+        List<Long> beatBegin = new ArrayList<>();
+        for(int i = 1; i < rPos.size(); i++) {
+            beatBegin.add(rPos.get(i) - Math.round(newRRInterval.get(i-1)*2.0/5));
+        }
+
+        // 打包
+        Map<String, Object> map = new HashMap<>();
+        map.put("RPos", rPos);
+        map.put("BeatBegin", beatBegin);
+        return map;
+    }
+
+    private static List<Float> diffFilter(List<Short> data) {
+        double[] b = {1,0,-2,0,1};
+        IDigitalFilter diffFilter = new FIRFilter(b);
+        diffFilter.createStructure(StructType.FIR_LPF);
+
+        List<Float> d2Data = new ArrayList<>();
+        for(Short d : data) {
+            d2Data.add((float)diffFilter.filter(d));
+        }
+
+        // 考虑滤波产生2个数的移位，矫正它
+        d2Data.remove(0);
+        d2Data.remove(0);
+        float tmp = d2Data.get(d2Data.size()-1);
+        d2Data.add(tmp);
+        d2Data.add(tmp);
+
+        return d2Data;
+    }
 }
